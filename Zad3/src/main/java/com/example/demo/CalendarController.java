68395b85a89920101d0e30cc9baf4982e4f232dd
rozer1 <robson.rs@vp.pl>
2019-12-10 18:27:59 +0100

    Added CalendarController

---
+++ Zad3/src/main/java/com/example/demo/CalendarController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/calendar")
+public class CalendarController {
+
+    @GetMapping
+    public boolean getCalendarForYearAndMonth(@RequestParam(name = "year") String year,
+                                              @RequestParam(name = "month") String month){
+        return true;
+    }
+}
